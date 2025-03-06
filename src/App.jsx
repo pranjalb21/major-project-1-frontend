190d4132ffc7a13bb8bf4443cc0ecd24e50f8bd3
Pranjal Gain <pranjalgain052@gmail.com>
2025-03-06 19:41:16 +0530

    feat: Initial setup done. Code cleanup done. Components, Contexts and Pages directory created

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.bundle.min.js"
+
+function App() {
+
+  return (
+    <>
+      <h1 className="fs-1">Hi</h1>
+    </>
+  )
+}
+
+export default App
