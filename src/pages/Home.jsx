dbb7cb4cb219dcf9697518a69ed5df251d098fcc
Pranjal Gain <pranjalgain052@gmail.com>
2025-03-11 19:35:12 +0530

    feat: Images added to hompage banners.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,47 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const categories = ["beauty", "fragrances", "furniture", "groceries"];
+const categories = [
+    {
+        id: 1,
+        name: "beauty",
+        image: "https://cdn.thewirecutter.com/wp-content/media/2024/06/drugstorebeautyproducts-2048px-2858.jpg",
+    },
+    {
+        id: 2,
+        name: "fragrances",
+        image: "https://www.arwenfragrances.com/wp-content/uploads/2020/08/per.png",
+    },
+    {
+        id: 3,
+        name: "furniture",
+        image: "https://5.imimg.com/data5/SELLER/Default/2021/12/VU/PJ/AP/51665645/all-furniture-product-design.jpg",
+    },
+    {
+        id: 4,
+        name: "groceries",
+        image: "https://sulabhmart.wordpress.com/wp-content/uploads/2013/07/1-composition-with-variety-of-grocery-products-t-monticello1.jpg",
+    },
+];
 export default function Home() {
     return (
         <main className="container mt-3">
             <section>
                 <div className="row">
-                    {categories.map((c, i) => (
-                        <div className="col-3 g-3" key={i}>
-                            <Link to={`/products/${c}`}>
-                                <div className="card position-relative">
+                    {categories.map((c) => (
+                        <div className="col-3 g-3" key={c.id}>
+                            <Link to={`/products/${c.name}`}>
+                                <div className="card position-relative overflow-hidden">
                                     <img
-                                        src={`https://placehold.co/400x300`}
-                                        alt=""
-                                        className="card-img-top img-fluid"
+                                        src={c.image}
+                                        alt={c.name}
+                                        className="img-fluid "
                                     />
                                     <p
-                                        className="position-absolute bg-white top-50  translate-middle-y text-center bg-opacity-50"
+                                        className="position-absolute bg-white top-50  translate-middle-y text-center bg-opacity-75"
                                         style={{ width: "100%" }}
                                     >
-                                        {c.toUpperCase()}
+                                        {c.name.toUpperCase()}
                                     </p>
                                 </div>
                             </Link>
@@ -29,13 +50,19 @@ export default function Home() {
                 </div>
             </section>
             <section className="mt-3">
-                <div className="">
+                <div className="position-relative">
                     <Link to={"/products/all"}>
                         <img
-                            src={`https://placehold.co/1400x600?text=All+Products`}
+                            src={`https://images.unsplash.com/photo-1515706886582-54c73c5eaf41?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`}
                             alt="Products"
                             className="img-fluid"
                         />
+                        <p
+                            className="position-absolute bg-white top-50  translate-middle-y text-center bg-opacity-75 py-3 text-black"
+                            style={{ width: "100%" }}
+                        >
+                            All Products
+                        </p>
                     </Link>
                 </div>
             </section>
